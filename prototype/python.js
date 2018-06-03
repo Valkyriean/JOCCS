17de0eb95819904d02e3c538b16bd5d9664c516e
WilliamMaa <williamma1998@outlook.com>
2018-06-04 01:11:06 +0800

    aaa

--- prototype/python.js
+++ prototype/python.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var spawn = require('child_process').spawn;
 var rmdir = require('./utils/rmdir');
 
-var input_0 = fs.readFileSync('./package/input.txt').toString();
+var input_0 = fs.readFileSync('input.txt').toString();
 var input_1 = input_0.split("\r\n");
 input_1.pop();
 var inputArr = [];
@@ -12,6 +12,18 @@ input_1.forEach(function(item) {
   item = trimSpace(item);
   input_2 = item.split(",");
   inputArr.push(input_2);
+})
+
+var output_0 = fs.readFileSync('output.txt').toString().trim();
+var output_1 = input_0.split("\r\n");
+output_1.pop();
+var outputArr = [];
+var output_2 = [];
+
+output_1.forEach(function(item) {
+  item = trimSpace(item);
+  output_2 = item.split(",");
+  outputArr.push(output_2);
 })
 
 var code = fs.readFileSync('test.py').toString();
@@ -31,6 +43,19 @@ function trimSpace(str) {
   var a = str.replace(/\s/g, "");
   return a;
 }
+
+function isEqual(arr_1, arr_2) {
+  if(arr_1.length != arr_2.length) return false;
+  if(typeof(arr_1[0]) == 'string' & typeof(arr_2[0]) == 'string') {
+    for(var i = 0; i < arr_1.length; i ++) {
+      if(arr_1[i] != arr_2[i]) return false;
+    }
+    return true;
+  } else {
+      for(var i = 0; i < arr_1.length; i ++) {a = isEqual(arr_1[i],arr_2[i])};
+      return a;
+  };
+};
 
 function abc(input, time, result) {
     var actualResult = result;
@@ -70,7 +95,8 @@ function abc(input, time, result) {
     //return result to server
     rmdir.rmdir(path);
     actualResult.splice(0,1);
-    console.log(actualResult);
+    var equal = isEqual(actualResult, outputArr);
+    console.log(equal);
   }
 }
 
