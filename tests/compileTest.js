2ea92ca410cb8f49ec6782cd2b7ad8886ed05f84
WilliamMaa <williamma1998@outlook.com>
2018-06-22 17:08:07 +0800

    update test

--- tests/compileTest.js
+++ tests/compileTest.js
@@ -1,7 +1,7 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let should = chai.should();
-// let expect = chai.expect;
+let expect = chai.expect;
 
 chai.use(chaiHttp);
 
@@ -17,8 +17,9 @@ describe('Compile Test',() =>{
               "code":"/**abc*/import java.util.Scanner;/*aaa*/public class test { public static void main(String[] args){Scanner s = new Scanner(System.in);String a = s.nextLine();String b = s.nextLine();System.out.println(a); System.out.println(b);}}"})
             .end((err, res) => {
                 res.body.should.have.status("success");
-                //res.body.should.have("test");
-                //expect(res.body).to.have.a.property({'result': 'test'})
+                //res.body.should.have.status({result: ["test","aaa"]});
+                //expect(res.body).to.have.a.property("result");
+                expect(res.body).to.have.deep.property('result', ["test","aaa"]);
                 done();
             });
     });
